8530921877e0bca99595d65402b273aa5a786a63
Nishthashree96 <Nishthashree95@gmain.com>
2020-08-14 23:14:12 +0530

    adding springboot jar file

---
+++ src/main/java/com/blog/BlogBackendApplication.java
@@ -0,0 +1,20 @@
+package com.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class BlogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBackendApplication.class, args);
+	}
+
+	@RequestMapping("/helloo")
+	public  String hello() {
+		return "Hello World!";
+	}
+}
